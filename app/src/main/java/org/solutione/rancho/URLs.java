cf666e1c1e723c2cbd2669a9069aff10a3d7749d
Zeratulbsd <rvilchiscruz@gmail.com>
2019-07-18 15:44:26 -0500

    urls

---
+++ app/src/main/java/org/solutione/rancho/URLs.java
@@ -0,0 +1,8 @@
+package org.solutione.rancho;
+
+public class URLs {
+    private static final String ROOT_URL = "http://35.243.185.249/ranchows/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "registro";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+}
