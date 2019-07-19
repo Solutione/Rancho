db6adb36bab0932ec40175af382fb8eb781c1c31
Zeratulbsd <rvilchiscruz@gmail.com>
2019-07-19 00:55:21 -0500

    login no funciona

---
+++ app/src/main/java/org/solutione/rancho/SharedPrefManager.java
@@ -0,0 +1,70 @@
+package org.solutione.rancho;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Belal on 9/5/2017.
+ */
+
+//here for this class we are using a singleton pattern
+
+public class SharedPrefManager {
+
+    //the constants
+    private static final String SHARED_PREF_NAME = "rancho";
+    private static final String KEY_USERNAME = "keyusername";
+    private static final String KEY_EMAIL = "keyemail";
+    private static final String KEY_PASS= "keypass";
+    private static final String KEY_ID = "keyid";
+    private static final String KEY_APE = "keyape";
+
+    private static SharedPrefManager mInstance;
+    private static Context mCtx;
+
+    private SharedPrefManager(Context context) {
+        mCtx = context;
+    }
+
+    public static synchronized SharedPrefManager getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new SharedPrefManager(context);
+        }
+        return mInstance;
+    }
+    public void userLogin(User user) {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(KEY_ID, user.getId());
+        editor.putString(KEY_USERNAME, user.getUsuario());
+        editor.putString(KEY_APE, user.getApellidos());
+        editor.putString(KEY_EMAIL, user.getEmail());
+        editor.putString(KEY_PASS, user.getPassword());
+        editor.apply();
+    }
+
+    public boolean isLoggedIn() {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(KEY_USERNAME, null) != null;
+    }
+
+    public User getUser() {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        return new User(
+                sharedPreferences.getInt(KEY_ID, -1),
+                sharedPreferences.getString(KEY_USERNAME, null),
+                sharedPreferences.getString(KEY_APE, null),
+                sharedPreferences.getString(KEY_EMAIL, null),
+                sharedPreferences.getString(KEY_PASS, null)
+        );
+    }
+
+    public void logout() {
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+      //  mCtx.startActivity(new Intent(mCtx, LoginActivity.class));
+    }
+}
--- app/src/main/java/org/solutione/rancho/User.java
+++ app/src/main/java/org/solutione/rancho/User.java
@@ -1,16 +1,18 @@
 package org.solutione.rancho;
 
-public class user {
+public class User {
     private int id;
     private String usuario,apellidos, email, password;
 
-    public user(int id, String usuario, String apellidos,String email, String password) {
+    public User(int id, String usuario, String apellidos,String email, String password) {
         this.id = id;
         this.usuario = usuario;
         this.apellidos=apellidos;
         this.email = email;
         this.password= password;
     }
+
+
 
     public int getId() {
         return id;
