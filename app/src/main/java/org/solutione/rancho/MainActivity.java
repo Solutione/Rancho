6163b793fb48ddf4086c8d277082278808e026d2
Jesus Sanchez <scysco@gmail.com>
2019-07-19 21:08:20 -0500

    base menu

--- app/src/main/java/org/solutione/rancho/MainActivity.java
+++ app/src/main/java/org/solutione/rancho/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         getSupportActionBar().hide();
+
+        ImageView imgMenu = (ImageView) findViewById(R.id.imgMenu);
+        final LinearLayout lytMenu = (LinearLayout) findViewById(R.id.lytMenu);
+        LinearLayout lytMenuClose = (LinearLayout) findViewById(R.id.lytMenuClose);
+
+        imgMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lytMenu.setVisibility(View.VISIBLE);
+            }
+        });
+
+        lytMenuClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lytMenu.setVisibility(View.GONE);
+            }
+        });
     }
 }
