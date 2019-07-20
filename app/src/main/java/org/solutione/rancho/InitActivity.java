2418212f2a5f7d29a3a88b029733b1c8d3c9861c
Jesus Sanchez <scysco@gmail.com>
2019-07-19 20:51:52 -0500

    splash and and main activity

---
+++ app/src/main/java/org/solutione/rancho/InitActivity.java
@@ -0,0 +1,30 @@
+package org.solutione.rancho;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
+public class InitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        TextView txtAcceder = (TextView) findViewById(R.id.txtAcceder);
+        txtAcceder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
