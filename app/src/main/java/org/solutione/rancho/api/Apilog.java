d75ae8c42354d672b7764764004d2c5bcfb257dc
unknown <sotero.reyes.gustavo@gmail.com>
2020-06-24 10:34:42 -0500

    Corrected Apilog

--- app/src/main/java/org/solutione/rancho/api/Apilog.java
+++ app/src/main/java/org/solutione/rancho/api/Apilog.java
@@ -1,25 +1,84 @@
 package org.solutione.rancho.api;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class Apilog {
-    int differenceDate(String date1, String date2 ) {
-        int difference=0;
 
-        SimpleDateFormat dateFormat= new SimpleDateFormat("dd/MM/yyy");
+    int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        try {
-            Date star_date=dateFormat.parse(date1);
-            Date end_date=dateFormat.parse(date2);
+    int differenceDate(String date1, String date2) {
+        int difference = 0;
+        int startD, startM, startY;
+        int endD, endM, endY;
 
-            difference=(int)((end_date.getTime()-star_date.getTime())/86400000);
+        String[] dateStart = date1.split("/");
+        startD = Integer.parseInt(dateStart[0]);
+        startM = Integer.parseInt(dateStart[1]);
+        startY = Integer.parseInt(dateStart[2]);
 
-        } catch (Exception e) {
+        String[] dateEnd = date2.split("/");
+        endD = Integer.parseInt(dateEnd[0]);
+        endM = Integer.parseInt(dateEnd[1]);
+        endY = Integer.parseInt(dateEnd[2]);
 
-        }
+        difference = countDays(startM - 1, endM - 1, startY, endY) + (endD - startD);
 
         return difference;
     }
 
+    int countDays(int month1, int month2, int year1, int year2) {
+        int days = 0;
+        if (year1 == year2) {
+            if (leap_year(year1)) {
+                months[1] = 29;
+            } else {
+                months[1] = 28;
+            }
+            for (int i = month1; i < month2; i++) {
+                days += months[i];
+                System.out.println(days);
+            }
+
+        } else {
+            int aux = 0;
+            for (int i = year1; i <= year2; i++) {
+            
+                if (leap_year(i)) {
+                    months[1] = 29;
+                } else {
+                    months[1] = 28;
+                }
+
+                if (i == year2) {
+                    for (int j = 0; j < month2; j++) {
+                        days += months[j];
+                        
+                    }
+                } else {
+                    if (aux == 0) {
+                        for (int j = month1; j < 12; j++) {
+                            days += months[j];
+                            
+                        }
+                    }else{
+                        for (int j = 0; j < 12; j++) {
+                            days += months[j];
+                            
+                        }
+                    }
+
+                    aux++;
+                }
+
+            }
+        }
+        return days;
+    }
+
+    boolean leap_year(int year) {
+        boolean status = false;
+        float aux = (float) year / 4;
+        if (aux % 1 == 0) {
+            status = true;
+        }
+        return status;
+    }
 }
